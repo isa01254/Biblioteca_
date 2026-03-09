1fbf85b4ffb786826cda2d5bc19b0082c7d6a7e2
Isadora Amaral Pereira <15157372680@MUZAMBINHO.EDU.BR>
2026-03-09 08:34:29 -0300

    commit2

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+class Cidade(models.Model):
+    nome = models.CharField(max_length=100, verbose_name = "Nome da cidade")
+    uf = models.CharField(max_length = 2, verbose_name = "UF")
+    def __str__(self):
+        return f"{self.nome}, {self.uf}"
+    class Meta:
+        verbose_name = "Cidade"
+        verbose_name_plural = "Cidades"
 
-# Create your models here.
+
+class Autor(models.Model):
+    nome = models.CharField(max_length = 100, verbose_name = "Nome do autor")
+    cidade =  models.ForeignKey(Cidade, on_delete=models.CASCADE, verbose_name="Cidade do autor")
+    def __str__(self):
+        return self.nome
+    class Meta:
+        verbose_name = "Autor"
+        verbose_name_plural = "Autores"
--- config/config/settings.py
+++ config/config/settings.py
@@ -63,7 +63,9 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.auth.context_processors.auth',
+
+'django.contrib.messages.context_processors.messages',            
             ],
         },
     },
@@ -77,8 +79,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'biblioteca_',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -105,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -118,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(STATIC_URL, "static")
+STATICFILES_DIRES = [
+    os.path.join(BASE_DIR, "app/static/"),
+]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
