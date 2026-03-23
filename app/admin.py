7c4f1d1fc2e28883063392ddbcccd4042b65f5ee
Isadora Amaral Pereira <15157372680@muzambinho.edu.br>
2026-03-23 07:38:11 -0300

    commit 5

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
-from .models import *
-from django.contrib import admin
+from .models import Cidade, Autor, Editora, Leitor, Livro, Genero
 
+# Definição dos Inlines (devem vir antes das classes que os utilizam)
+class LivroInline(admin.TabularInline):
+    model = Livro
+    extra = 1 
+
+#Configuração customizada do Autor
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+    inlines = [LivroInline]
+
+#Registro dos Modelos
+# Note que o 'Autor' agora é registrado junto com sua classe de configuração 'AutorAdmin'
+admin.site.register(Autor, AutorAdmin)
+
+# Registro simples para os outros modelos
 admin.site.register(Cidade)
-admin.site.register(Autor)
 admin.site.register(Editora)
 admin.site.register(Leitor)
 admin.site.register(Livro)
